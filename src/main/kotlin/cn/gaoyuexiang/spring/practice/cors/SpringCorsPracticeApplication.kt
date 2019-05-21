1c2242c1ab1ae014c266d9023ec8959903051e87
kbyyd24 <melo@gaoyuexiang.cn>
2019-05-21 21:44:38 +0800

    init

---
+++ src/main/kotlin/cn/gaoyuexiang/spring/practice/cors/SpringCorsPracticeApplication.kt
@@ -0,0 +1,11 @@
+package cn.gaoyuexiang.spring.practice.cors
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCorsPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCorsPracticeApplication>(*args)
+}
