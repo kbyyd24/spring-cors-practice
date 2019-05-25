e65de928bff13627b97b3207e62066ca23d799d0
kbyyd24 <melo@gaoyuexiang.cn>
2019-05-26 03:48:56 +0800

    config security, set http basic config, so that request passed cors filter can go through controller.

--- src/test/kotlin/cn/gaoyuexiang/spring/practice/cors/controller/HelloControllerTest.kt
+++ src/test/kotlin/cn/gaoyuexiang/spring/practice/cors/controller/HelloControllerTest.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders.ORIGIN
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -16,17 +17,22 @@ internal class HelloControllerTest(@Autowired val mockMvc: MockMvc) {
 
     private val api = "/hello"
 
+    private val username = "cors-tester"
+
+    private val password = "tester"
+
     @Test
     internal fun shouldGetResponseSuccessWithoutOriginHeader() {
-        this.mockMvc.perform(get(api))
+        this.mockMvc.perform(get(api).with(httpBasic(username, password)))
                 .andExpect(status().isOk)
                 .andExpect(content().string("Hello, CORS!"))
     }
 
     @Test
     internal fun shouldGetResponseSuccessWithAllowedOrigin() {
-        this.mockMvc.perform(get(api).header(ORIGIN, "http://localhost:8080"))
+        this.mockMvc.perform(get(api).header(ORIGIN, "http://localhost:8080").with(httpBasic(username, password)))
                 .andExpect(status().isOk)
+                .andExpect(content().string("Hello, CORS!"))
     }
 
     @Test
