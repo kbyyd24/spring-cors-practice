9f270babbe4d7980d4fe8c9e7825f9976ed04f87
kbyyd24 <melo@gaoyuexiang.cn>
2019-05-26 03:03:26 +0800

    import spring security, which will check authentication before cors check by default.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
