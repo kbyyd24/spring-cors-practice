1094e4c915d5407d12508b2b536684a550e8c561
kbyyd24 <melo@gaoyuexiang.cn>
2019-05-30 22:31:15 +0800

    update test, fixed the http://localhost issue
    https://github.com/spring-projects/spring-boot/issues/17007

--- src/test/kotlin/cn/gaoyuexiang/spring/practice/cors/controller/HelloControllerTest.kt
+++ src/test/kotlin/cn/gaoyuexiang/spring/practice/cors/controller/HelloControllerTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpHeaders.ORIGIN
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic
 import org.springframework.test.web.servlet.MockMvc
@@ -13,7 +14,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-internal class HelloControllerTest(@Autowired val mockMvc: MockMvc) {
+internal class HelloControllerTest(@Autowired val mockMvc: MockMvc, @LocalServerPort val port: Int) {
 
     private val api = "/hello"
 
@@ -37,7 +38,7 @@ internal class HelloControllerTest(@Autowired val mockMvc: MockMvc) {
 
     @Test
     internal fun shouldGetForbiddenWithNotAllowedOrigin() {
-        this.mockMvc.perform(get(api).header(ORIGIN, "http://baidu.com"))
+        this.mockMvc.perform(get("http://localhost:$port$api").header(ORIGIN, "http://localhost"))
                 .andExpect(status().isForbidden)
                 .andExpect(content().string("Invalid CORS request"))
     }
